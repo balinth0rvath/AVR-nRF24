2f04bec15028ff536ee4b4e5ede8da434e53dca2
balinth0rvath <horvath0balint@gmail.com>
2021-04-30 15:11:51 +0200

    Setup Atmel Studio

---
+++ AVR-nRF24/main.c
@@ -0,0 +1,17 @@
+/*
+ * AVR-nRF24.c
+ *
+ * Created: 4/30/2021 3:04:23 PM
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
