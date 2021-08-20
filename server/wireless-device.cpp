412e3ad036c18f5f8296424439560e90f529e2f6
horvath0balint <horvath0balint@gmail.com>
2021-08-20 13:23:06 +0200

    server: nrf24 device impl skeleton

--- server/wireless-device.cpp
+++ server/wireless-device.cpp
@@ -1,2 +1,71 @@
 #include "wireless-device.h"
+#include <cstdlib>
+#include <fcntl.h>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <exception>
+
+#define NRF24_IOCTL_SET_RECEIVER _IO(153,0)
+#define NRF24_IOCTL_SET_TRANSMITTER _IO(153,1)
+#define NRF24_IOCTL_SET_ADDRESS _IOW(153,2, struct ADDRESS_T)
+
+NRF24Device::NRF24Device()
+{
+  fd = open(deviceName, O_RDWR);
+  if (fd==0)
+  {
+    throw std::exception();
+  }
+}
+
+NRF24Device::~NRF24Device()
+{
+  
+  int ret = close(fd);
+  if (fd!=0)
+  {
+    std::cout << "~NRF24Device close failed\n";
+  } 
+}
+
+void NRF24Device::setReceiver()
+{
+  int ret = ioctl(fd, NRF24_IOCTL_SET_RECEIVER, 0);
+  if (ret == -1)
+  {
+    throw std::exception();
+  }
+}
+
+void NRF24Device::setTransmitter()
+{
+  int ret = ioctl(fd, NRF24_IOCTL_SET_TRANSMITTER, 0);
+  if (ret == -1)
+  {
+    throw std::exception();
+  }
+}
+
+void NRF24Device::setAddress(const std::vector<char>& address)
+{
+/*
+  struct ADDRESS_T {
+    unsigned char octet[5];
+  } octets;
+*/
+}
+
+int NRF24Device::send(const std::vector<char>& data)
+{
+  int retval;
+  return retval;
+
+}
+std::vector<char> NRF24Device::receive()
+{
+  std::vector<char> data {};
+
+  return data;
+}
 
--- server/wireless-device.h
+++ server/wireless-device.h
@@ -1,10 +1,26 @@
+#include <vector>
+#include <iostream>
+
 class WirelessDevice {
 public:
-  virtual void open() = 0;
-  virtual void close() = 0;
   virtual void setReceiver() = 0;
   virtual void setTransmitter() = 0;
-  virtual void setAddress() = 0;
+  virtual void setAddress(const std::vector<char>& address) = 0;
+  virtual int send(const std::vector<char>& data) = 0;
+  virtual std::vector<char> receive() = 0;
+  virtual ~WirelessDevice() { std::cout << "WirelessDevice Base destructor called! \n";};
+};
+
+class NRF24Device : public WirelessDevice {
+public:
+  NRF24Device();
+  ~NRF24Device();
+  virtual void setReceiver();
+  virtual void setTransmitter();
+  virtual void setAddress(const std::vector<char>& address);
+  virtual int send(const std::vector<char>& data);
+  virtual std::vector<char> receive();
 private:
+  static constexpr const char * deviceName = "/dev/nrf24d";
   int fd = 0;
 };
