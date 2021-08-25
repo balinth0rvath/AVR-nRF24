523918b7f2f9da5909539c0427c9ee60184f58f4
bhorvath2 <balint.horvath2@nng.com>
2021-08-25 16:47:41 +0200

    mcu: Implement one time dummy sender. Separate it from test longloop

--- AVR-nRF24/main.c
+++ AVR-nRF24/main.c
@@ -4,6 +4,9 @@
  * Created: 4/30/2021 3:04:23 PM
  */ 
 
+ #define ONE_PACKET 1
+ #define TEST_LONGLOOP 0
+
 #include "common.h"
 #include <avr/io.h>
 #include <util/delay.h>
@@ -20,6 +23,22 @@ void send_response(void)
 	payload[1] = p.source_address;
 	payload[2] = p.value;
 	payload[3] = p.aux;
+	nrf24_transmit_packet(payload, &status);	
+	_delay_ms(100);	
+		
+	nrf24_transmit_packet(payload, &status);	
+	_delay_ms(100);	
+	
+}
+
+void send_dummy_packet(void)
+{	
+	uint8_t status;			
+	char payload[4] = {};
+	payload[0] = 54;
+	payload[1] = 55;
+	payload[2] = 53;
+	payload[3] = 54;
 	nrf24_transmit_packet(payload, &status);	
 	_delay_ms(100);	
 		
@@ -50,10 +69,14 @@ int main(void)
 	blink(2,0);
 	nrf24_init(0);	
 	ret = nrf24_check_device();
+	/*
 	if (ret!=0)
 	{
 		blink(10,0);
 	}
+	*/
+
+
 
 	uint8_t address[5] = {0x10, 0x20, 0x30, 0x40, 0x50};
 	
@@ -65,6 +88,17 @@ int main(void)
 	EIMSK |= (1 << INT0);
 	sei();	
 	
+#ifdef ONE_PACKET
+	nrf24_set_transmitter();
+	_delay_ms(10);
+	send_dummy_packet();
+	_delay_ms(10);
+	while(1);
+	
+#endif //ONE_PACKET
+
+#ifdef TEST_LONGLOOP
+	nrf24_set_receiver();	
 	while(1)	
 	{		
 		_delay_ms(10);
@@ -79,6 +113,7 @@ int main(void)
 			nrf24_set_receiver();	
 		}
 	}	
+#endif //TEST_LONGLOOP
 }
 
 ISR (INT0_vect)
