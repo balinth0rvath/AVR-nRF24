688d651291bdd86e57a909854f042105a8fa10dd
horvath0balint <horvath0balint@gmail.com>
2021-09-16 16:00:45 +0200

    server: Create skeleton of common server/unit test structure

--- server/controller.h
+++ server/controller.h
@@ -1,5 +1,5 @@
 #include <memory>
-#include "wireless-device.h"
+#include "lib/wireless-device.h"
 
 class Controller {
 public:
---
+++ server/test.cpp
@@ -0,0 +1,9 @@
+#include "controller.h"
+
+int main()
+{
+  Controller controller {};  
+
+  return 0;
+}
+
