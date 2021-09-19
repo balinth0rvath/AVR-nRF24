cf43916c24a7cfc403cd1490ee345ae9aac40d1f
horvath0balint <horvath0balint@gmail.com>
2021-09-19 11:14:00 +0200

    server: Move tests under server directory. Delete old tests

--- test/nrf24-lib.h
+++
@@ -1,5 +0,0 @@
-void nrf24_lib_func(void);
-int nrf24_lib_set_transmitter(int fd);
-int nrf24_lib_set_receiver(int fd);
-int nrf24_lib_open();
-int nrf24_lib_set_address(int fd, unsigned char* address);
--- test/nrf24-test-cases.h
+++
@@ -1,5 +0,0 @@
-#define SLEEP_US 1300000 
-int test_longloop(int* fd, int* error, char* message);
-int test_temperature(int* fd, int* error, char* message);
-int test_addresses(int* fd, int* error, char* message);
-
