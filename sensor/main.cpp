34a7d38a065dfe39f9ce872532d34d716e776672
balinth0rvath <horvath0balint@gmail.com>
2021-10-23 15:52:18 +0200

    mcu: Create new project for c++ solution

---
+++ sensor/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * sensor.cpp
+ *
+ * Created: 10/23/2021 3:49:46 PM
+ * Author : balinth0horvath
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
